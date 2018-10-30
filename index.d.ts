6b69dcac957500fed84bbd5d46091ed09492bc2c
chen <tianclch@gmail.com>
2018-10-30 00:48:51 -0400

    add libraries files

---
+++ index.d.ts
@@ -0,0 +1,62 @@
+export interface ServiceConfiguration {
+  authorizationEndpoint: string;
+  tokenEndpoint: string;
+  revocationEndpoint?: string;
+  registrationEndpoint?: string;
+}
+
+export type BaseAuthConfiguration =
+  | {
+      clientId: string;
+      issuer?: string;
+      serviceConfiguration: ServiceConfiguration;
+    }
+  | {
+      clientId: string;
+      issuer: string;
+      serviceConfiguration?: ServiceConfiguration;
+    };
+
+interface BuiltInParameters {
+  display?: "page" | "popup" | "touch" | "wap";
+  login_prompt?: string;
+  prompt?: "consent" |"login" | "none" | "select_account";
+}
+
+export type AuthConfiguration = BaseAuthConfiguration & {
+  clientSecret?: string;
+  scopes: string[];
+  redirectUrl: string;
+  additionalParameters?: BuiltInParameters & { [name: string]: string };
+  dangerouslyAllowInsecureHttpRequests?: boolean;
+};
+
+export interface AuthorizeResult {
+  accessToken: string;
+  accessTokenExpirationDate: string;
+  additionalParameters?: { [name: string]: string };
+  idToken: string;
+  refreshToken: string;
+  tokenType: string;
+}
+
+export interface RevokeConfiguration {
+  tokenToRevoke: string;
+  sendClientId?: boolean;
+}
+
+export interface RefreshConfiguration {
+  refreshToken: string;
+}
+
+export function authorize(config: AuthConfiguration): Promise<AuthorizeResult>;
+
+export function refresh(
+  config: AuthConfiguration,
+  refreshConfig: RefreshConfiguration
+): Promise<AuthorizeResult>;
+
+export function revoke(
+  config: BaseAuthConfiguration,
+  revokeConfig: RevokeConfiguration
+): Promise<void>;
